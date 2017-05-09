512e5477383917e4dc40a6c733f37de6057fdbd2
yuw178 <yuw178@ucsd.edu>
2017-05-09 10:14:48 -0700

    Week 5
    Used this code to add to my blast output

---
+++ formatblast.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+with open('blast1_atvec.out') as infile:
+    alllines = [string.strip().split('\t') for string in infile.readlines()]
+    for line in alllines:
+        line = line.append(str(int(line[8])/int(line[3])))
+with open('blast1_atvec.out','w') as outfile:
+    for line in alllines:
+        outfile.write('\t'.join(map(str,line))+'\n')
